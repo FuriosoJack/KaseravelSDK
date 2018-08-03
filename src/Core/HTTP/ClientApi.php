ed50aa533142ab4dff904cb16f1dbc1c2d75be72
FuriosoJack <iam@furiosojack.com>
2018-08-03 13:08:43 -0500

    Se añades nuevas clases del core

---
+++ src/Core/HTTP/ClientApi.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace FuriosoJack\Kaseravel\Core\HTTP;
+
+/**
+ * Class ClientApi
+ * @package FuriosoJack\Kaseravel\Core\HTTP\Request
+ * @author Juan Diaz - FuriosoJack <iam@furiosojack.com>
+ */
+class ClientApi
+{
+    const API_BASE_PATH = "/api/v1.0/";
+
+    private $restClient;
+
+    public function __construct(string $host, string $user, string $password)
+    {
+
+    }
+
+}
---
+++ src/Core/HTTP/Response.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace FuriosoJack\Kaseravel\Core\HTTP;
+
+/**
+ * Class Response
+ * @package FuriosoJack\Kaseravel\Core\HTTP
+ * @author Juan Diaz - FuriosoJack <iam@furiosojack.com>
+ */
+class Response
+{
+    private $result;
+
+
+    public function __construct($result)
+    {
+        $this->result;
+    }
+
+    public function getError()
+    {
+
+    }
+
+    public function getResult()
+    {
+
+    }
+
+    public function existError():bool
+    {
+
+    }
+
+
+
+
+}
