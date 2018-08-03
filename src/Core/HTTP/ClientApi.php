fc6c25a6d7cf5f25857fd35aa7124a011c9009f3
FuriosoJack <iam@furiosojack.com>
2018-08-03 14:38:34 -0500

    Se añaden agunos metodos del cliente

--- src/Core/HTTP/ClientApi.php
+++ src/Core/HTTP/ClientApi.php
@@ -16,7 +16,36 @@ class ClientApi
 
     public function __construct(string $host, string $user, string $password)
     {
+        $this->createRestClient($host,$user,$password);
 
     }
+
+    private function createRestClient(string $host, string $user, string $password)
+    {
+        $url = 'https://'. $host . self::API_BASE_PATH;
+        $this->restClient = new \RestClient([
+            'base_url' => $url
+        ]);
+
+    }
+
+    private function getUrl()
+    {
+
+    }
+
+    public function execute(string $host, string $path, array $parameters = [], array $headers = []): Response
+    {
+
+    }
+
+
+
+    private function getOptionsRestClient()
+    {
+
+    }
+
+
 
 }
